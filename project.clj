3736cadc00cbeda478b05b1ef7738240ef9d0425
ron.huafeng@gmail.com <ron.huafeng@gmail.com>
2015-11-29 11:02:08 +0800

    Update Clojure source path

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
+  :source-paths ["src"]
   :global-vars {*warn-on-reflection* true
                 *assert* false}
   :dependencies [[org.clojure/clojure "1.5.1"]]
