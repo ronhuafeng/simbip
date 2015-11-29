7ce98737625126dab1d43c99285291b90a861406
ron.huafeng@gmail.com <ron.huafeng@gmail.com>
2015-11-29 12:48:12 +0800

    Try Travis

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :aot :all
+ ; :aot :all
   :source-paths ["src"]
   :global-vars {*warn-on-reflection* true
                 *assert* false}
